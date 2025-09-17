0fe12ace8a2094ead6a5378c8658792bc6a50533
alejosanca84 <alsanta1984@gmail.com>
2025-09-17 02:04:52 -0500

    Practica 3 punto 3
    esta es la practica 3 punto 3

---
+++ Practica_3/Practica_3_punto3/Core/Src/main.c
@@ -0,0 +1,296 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "API_delay.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	// Arreglo constante que define la secuencia de tiempos de ENCENDIDO
+	const uint32_t TIEMPOS[] = {500, 100, 100, 1000};
+
+	// Variable de estado para saber en qué paso de la secuencia estamos
+	uint8_t sequence_index = 0;
+
+	// Variable de estado para saber si el LED está encendido o apagado
+	// Se inicia en false, ya que el LED empieza apagado.
+	bool_t led_is_on = false;
+
+	// Nuestra única variable de retardo
+	delay_t led_delay;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  // Calculamos el número total de pasos en nuestra secuencia
+  const uint8_t sequence_size = sizeof(TIEMPOS) / sizeof(TIEMPOS[0]);
+
+  // Inicializamos el retardo con la duración del PRIMER paso de la secuencia
+  delayInit(&led_delay, TIEMPOS[sequence_index]);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if ( delayRead(&led_delay) )
+    {
+        // Cada vez que el retardo se cumple, invertimos el estado del LED
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+
+        // Actualizamos nuestra variable de estado del LED
+        led_is_on = !led_is_on;
+
+        // --- AQUÍ ESTÁ LA LÓGICA CLAVE DE LA MÁQUINA DE ESTADOS ---
+
+        // Si acabamos de APAGAR el LED, significa que un ciclo completo (ON-OFF) ha terminado.
+        // Es el momento de prepararse para el SIGUIENTE ciclo con un nuevo tiempo.
+        if (!led_is_on)
+        {
+            // Avanzamos al siguiente tiempo en la secuencia
+            sequence_index++;
+
+            // Si llegamos al final de la secuencia, volvemos al inicio
+            if (sequence_index >= sequence_size)
+            {
+                sequence_index = 0;
+            }
+
+            // --- CAMBIO AQUÍ ---
+                        // Verificamos que el retardo NO esté corriendo ANTES de cambiar su duración.
+                        // Esta es una medida de seguridad para una API robusta.
+                        if (!delayIsRunning(&led_delay))
+                        {
+                            delayWrite(&led_delay, TIEMPOS[sequence_index]);
+                        }
+        }
+        // Si acabamos de ENCENDER el LED, no hacemos nada.
+        // Dejamos que el retardo se complete de nuevo con la MISMA duración
+        // para cumplir con el ciclo de trabajo del 50%.
+    }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Practica_3/Practica_3_punto3/Drivers/API/Src/API_delay.c
@@ -0,0 +1,61 @@
+#include "API_delay.h"
+#include "stm32f4xx_hal.h" // Necesario para la función HAL_GetTick()
+
+/**
+  * @brief  Inicializa una estructura de retardo.
+  * @param  delay: Puntero a la estructura de retardo.
+  * @param  duration: Duración del retardo en milisegundos (ms).
+  * @retval None
+  */
+void delayInit( delay_t * delay, tick_t duration )
+{
+	delay->duration = duration;
+	delay->running = false;
+}
+
+/**
+  * @brief  Lee el estado del retardo. Si no está corriendo, lo inicia.
+  *         Si está corriendo, verifica si se ha cumplido el tiempo.
+  * @param  delay: Puntero a la estructura de retardo.
+  * @retval bool_t: true si el tiempo se cumplió, false en caso contrario.
+  */
+bool_t delayRead( delay_t * delay )
+{
+	bool_t time_cumplido = false;
+
+	if( !delay->running )
+	{
+		delay->startTime = HAL_GetTick();
+		delay->running = true;
+	}
+	else
+	{
+		if( (HAL_GetTick() - delay->startTime) >= delay->duration )
+		{
+			time_cumplido = true;
+			delay->running = false;
+		}
+	}
+
+	return time_cumplido;
+}
+
+/**
+  * @brief  Permite actualizar la duración de un retardo existente.
+  * @param  delay: Puntero a la estructura de retardo.
+  * @param  duration: Nueva duración del retardo en milisegundos (ms).
+  * @retval None
+  */
+void delayWrite( delay_t * delay, tick_t duration )
+{
+	delay->duration = duration;
+}
+/**
+  * @brief  Devuelve el estado actual del flag 'running' de un retardo.
+  * @param  delay: Puntero a la estructura de retardo.
+  * @retval bool_t: true si el retardo está actualmente en conteo, false en caso contrario.
+  */
+bool_t delayIsRunning( delay_t * delay )
+{
+	return delay->running;
+}
